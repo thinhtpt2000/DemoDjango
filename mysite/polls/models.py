cb2558d8fdc886f0c7ba3310341cbabc1020ad8c
thinhtpt2000 <thinhtpt.2000@gmail.com>
2021-02-19 07:45:15 +0700

    add models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 # Ở đây chứa/định nghĩa các models/thực thể khi dùng với DB, ví dụ như Student
+
+class Question(models.Model):
+    question_text  = models.CharField(max_length=200)
+    time_public = models.DateTimeField()
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    vote = models.IntegerField(default=0)
