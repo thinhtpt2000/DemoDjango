45cb5e74b8d3b12243065c57cfa8089da2341f17
thinhtpt2000 <thinhtpt.2000@gmail.com>
2021-03-02 22:57:20 +0700

    update with namespace - app_name

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views # để liên kết url với view tương ứng, ta cần import views (from thư mục hiện tại, ta import views)
 
+app_name = "polls"
+
 urlpatterns = [
     path('<int:q_id>', views.vote, name="vote"),
     path('detail/<int:question_id>', views.viewDetail, name="view_detail"),
